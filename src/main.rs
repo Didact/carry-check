1d96df3d54031896e695289ddf6362f06ca116f9
Dakota Smith <dsmith@appareo.com>
2017-06-27 21:02:38 -0700

    add fn to account id from gamertag

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,8 @@ use hyper_tls::{HttpsConnector};
 use tokio_core::reactor::{Core};
 use serde_json::{Deserializer, Value};
 use std::result::{Result};
+use std::option::{Option};
+use std::boxed::{Box};
 
 header! { (XBnetApiHeader, "X-API-Key") => [String] }
 
@@ -46,11 +48,25 @@ impl Display for CharacterId {
         self.0.fmt(f)
     }
 }
+
+fn get_account_id<CC>(platform: PlatformType, display_name: &str, client: &Client<CC>) -> Box<Future<Item=Option<AccountId>, Error=hyper::Error>>
+where CC: hyper::client::Connect {
+    let uri = format!("https://www.bungie.net/Platform/Destiny/SearchDestinyPlayer/{membershipType}/{displayName}/", membershipType=platform, displayName=display_name);
+    let mut req = Request::new(Method::Get, uri.parse().unwrap());
+    req.headers_mut().set(XBnetApiHeader(String::from("")));
+    Box::new(client.request(req).and_then(|res| {
+        res.body().concat2().map(|body| {
+            serde_json::from_slice::<Value>(&body).as_ref().ok().and_then(|v| v.get("Response")).and_then(|r| r.as_array()).and_then(|a| a.first()).and_then(|r| r.get("membershipId")).and_then(|m| m.as_str()).map(String::from).map(|id| AccountId(id))
+        })
+    }))
+}
+
 fn get_account_summary<CC>(platform: PlatformType, account: &AccountId, client: &Client<CC>) -> FutureResponse 
 where CC: hyper::client::Connect {
     let uri = format!("https://www.bungie.net/Platform/Destiny/{membershipType}/Account/{destinyMembershipId}/Summary/", membershipType=platform, destinyMembershipId=account);
     let mut req = Request::new(Method::Get, uri.parse().unwrap());
     req.headers_mut().set(XBnetApiHeader(String::from("")));
+    println!("ayy");
     client.request(req)
 }
 
@@ -61,9 +77,11 @@ fn main() {
     	.connector(HttpsConnector::new(4, handle).unwrap())
     	.build(handle);
 
-    let work = get_account_summary(PlatformType::Psn, &AccountId("4611686018428490898".to_owned()), &client).and_then(|res| {
-        res.body().concat2().map(|body| {
-            io::stdout()
+    let work = get_account_id(PlatformType::Psn, "King_Cepheus", &client).and_then(|id| {
+        get_account_summary(PlatformType::Psn, &id.unwrap(), &client).and_then(|res| {
+            res.body().concat2().map(|body| {
+                io::stdout().write_all(&body)
+             })
         })
     });
     core.run(work).unwrap();
